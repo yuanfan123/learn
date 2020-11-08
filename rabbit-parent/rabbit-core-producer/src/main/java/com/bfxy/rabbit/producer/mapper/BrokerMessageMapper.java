f8026d7d959d018a303e0705bea355626829fe0b
Novice Wang <81863046@qq.com>
2020-11-08 23:54:41 +0800

    elastic-job

--- rabbit-parent/rabbit-core-producer/src/main/java/com/bfxy/rabbit/producer/mapper/BrokerMessageMapper.java
+++ rabbit-parent/rabbit-core-producer/src/main/java/com/bfxy/rabbit/producer/mapper/BrokerMessageMapper.java
@@ -21,7 +21,7 @@ public interface BrokerMessageMapper {
 
     int updateByPrimaryKeySelective(BrokerMessage record);
 
-    int updateByPrimaryKeyWithBLOBs(BrokerMessage record);
+//    int updateByPrimaryKeyWithBLOBs(BrokerMessage record);
 
     int updateByPrimaryKey(BrokerMessage record);
 	
